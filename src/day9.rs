ff54e928ba7346ffe90da08c8b9e136c31be29eb
giulio <giuliop@gmail.com>
2020-12-28 21:39:35 +0100

    refactoring

--- src/day9.rs
+++ src/day9.rs
@@ -4,7 +4,10 @@ use itertools::Itertools;
 const OFFSET: usize = 25;
 
 fn combinations(nums: &[usize]) -> Vec<usize> {
-    nums.iter().combinations(2).map(|x| *x[0] + *x[1]).collect()
+    nums.iter()
+        .combinations(2)
+        .map(|x| x.iter().copied().sum())
+        .collect()
 }
 
 fn invalid_number(nums: &Vec<usize>) -> usize {
@@ -22,26 +25,25 @@ pub fn a() -> String {
 }
 
 // return the start and end index od the contiguous set that sums to n in nums
-fn contiguous_set(n: usize, nums: &Vec<usize>) -> (usize, usize) {
+fn contiguous_set(n: usize, nums: &Vec<usize>) -> Option<(usize, usize)> {
     for start in 0..nums.len() {
         for end in start + 1..nums.len() {
             match nums[start..end].iter().sum::<usize>() {
-                x if x == n => return (start, end),
+                x if x == n => return Some((start, end)),
                 x if x > n => break,
                 _ => (),
             }
         }
     }
-    (0, 0)
+    None
 }
 
 pub fn b() -> String {
     let nums = read_input::<usize>("../input/day9");
     let n = invalid_number(&nums);
 
-    let (start, end) = contiguous_set(n, &nums);
+    let (start, end) = contiguous_set(n, &nums).unwrap();
+    let range = &nums[start..end];
 
-    (**&nums[start..end].iter().max().unwrap()
-        + **&nums[start..end].iter().min().unwrap())
-        .to_string()
+    (range.iter().max().unwrap() + range.iter().min().unwrap()).to_string()
 }
